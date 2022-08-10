54e85cdeb38c8bc735922fd6fc81bffa3d9b5a28
avinaw20 <avinaw20@gmail.com>
2022-08-10 18:04:19 +0530

    working  public and private chat  rooms

--- room/models.py
+++ room/models.py
@@ -1,7 +1,16 @@
 from unicodedata import name
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# class Message(models.Model):
+#     message=models.TextField(max_length=100)
+#     user=models.OneToOneField(User, on_delete=models.CASCADE)
 class Room(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
+    # members=models.ManyToManyField(User)
+    # messages=models.ForeignKey(Message,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
